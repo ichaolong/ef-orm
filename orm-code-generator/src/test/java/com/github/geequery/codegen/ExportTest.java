b2360d0449190a5ffd761508c124733364ba6324
Joey <mr.jiyi@gmail.com>
2017-09-19 18:57:57 +0800

    1、关于跳过内部类扫描功能修正。
    2、继续测试关于数据库初始化功能。

--- orm-code-generator/src/test/java/com/github/geequery/codegen/ExportTest.java
+++ orm-code-generator/src/test/java/com/github/geequery/codegen/ExportTest.java
@@ -3,13 +3,16 @@ package com.github.geequery.codegen;
 import java.io.File;
 
 import jef.database.DbClient;
+import jef.database.datasource.SimpleDataSource;
 
 import org.junit.Test;
 
 public class ExportTest {
+    DbClient db=new DbClient(new SimpleDataSource("jdbc:mysql://api.hikvision.com.cn:3306/api", "root", "88075998"));
 	@Test
 	public void test123() throws Exception{
 		DbExporter ex=new DbExporter();
-		ex.doxport(new DbClient(), "",new File("src/main/resources"));
+		ex.doxport(db, "com.github.geequery.codegen.entity",new File("src/main/resources"));
+		
 	}
 }
